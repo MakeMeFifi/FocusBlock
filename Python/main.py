920ba954ebaa8a5871e635ca2610d5a61641487a
Chris Mikolajek <chrismikolajek2006@gmail.com>
2026-03-12 15:57:05 +0100

    Testing first time psutil

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,18 @@
-import psutil as ps
-print(ps.cpu_times())
+import psutil as ps # libary to Stop programss 
+import time
+
+blockedSoftware = ["steam","whatsapp"] #List with all the blocked Sodtware
+
+
+
+
+while True:
+    time.sleep(5)   # checkt only every 5 seconds so that 
+    print("running")
+    for software in ps.process_iter() :     #goes every software
+        try:
+            if software.name() in blockedSoftware:
+                software.kill()
+                break
+        except:
+            pass
