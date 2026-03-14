4e544fc0fe3a0431e3ce0b9dc1ad504a6a2eaf2c
MakeMeFifi <chrismikolajek2006@gmail.com>
2026-03-14 10:52:52 +0100

    Debugging improved

--- Python/main.py
+++ Python/main.py
@@ -1,18 +1,18 @@
-import psutil as ps # libary to Stop programss 
 import time
 
-blockedSoftware = ["steam","whatsapp"] #List with all the blocked Sodtware
+import psutil as ps  # libary to Stop programss
 
-
+blockedSoftware = ["steam", "whatsapp", "dolphin"]  # List with all the blocked Sodtware
 
 
 while True:
-    time.sleep(5)   # checkt only every 5 seconds so that 
+    time.sleep(5)  # checkt only every 5 seconds so that
     print("running")
-    for software in ps.process_iter() :     #goes every software
+    for software in ps.process_iter():  # goes every software
         try:
             if software.name() in blockedSoftware:
                 software.kill()
+                print(f"Die Software: {software.name()} wurde beendet")
                 break
-        except:
+        except Exception:
             pass
