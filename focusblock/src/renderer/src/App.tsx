1fb6f90612a1d7ce3fa3ba58ae2516603951a498
MakeMeFifi <chrismikolajek2006@gmail.com>
2026-03-14 21:40:34 +0100

    Added in main the Overview and changed the layout to a Bootstrap orientated Layout

--- focusblock/src/renderer/src/App.tsx
+++ focusblock/src/renderer/src/App.tsx
@@ -1,13 +1,25 @@
 import Navbar from './components/Navbar'
+import Overview from './components/Overview'
 
 function App(): React.JSX.Element {
   return (
-    <div className="bg-dark vh-100 text-white body">
+    <>
       <Navbar />
-      <div className="d-flex justify-content-center align-items-center">
-        <h1>Hello World</h1>
+      <div className="bg-dark vh-100 text-white body">
+        <main className="container fluid p-4">
+          <div className="mx-auto" style={{ maxWidth: '1400px' }}>
+            <div className="mb-4">
+              <Overview
+                numberOfBlockedApps={4}
+                taskNumber={3}
+                uploadToday={2}
+                numberOfTeamMembers={7}
+              />
+            </div>
+          </div>
+        </main>
       </div>
-    </div>
+    </>
   )
 }
 
